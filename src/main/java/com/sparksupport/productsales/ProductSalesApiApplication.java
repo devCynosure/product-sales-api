823a8c5a5b9845baaaa0546409e6fec3abde6384
unknown <devCynosure>
2025-03-29 14:39:42 +0530

    first commit

---
+++ src/main/java/com/sparksupport/productsales/ProductSalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparksupport.productsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSalesApiApplication.class, args);
+	}
+
+}
